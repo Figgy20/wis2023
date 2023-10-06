b28dbd5933e5c465bc4f739a62a6e7dc8276bc27
Figgy20 <keaganswanepoel10d@gmail.com>
2023-10-06 20:58:16 +0800

    lesson completed

---
+++ Lesson3.php
@@ -0,0 +1,14 @@
+<?php
+    echo "I'm about to learn PHP!<br>";
+    # test comment
+    // this is also a comment wow
+    /*
+        This comment goes through multiple lines
+    */
+    print "what does this do? :o <br>";
+    print "same as echo i think <br>";
+    $number = 5;
+    print "the number is $number <br>";
+    print "the number is $numBEr <br>";
+    print "This is cool";
+?>
